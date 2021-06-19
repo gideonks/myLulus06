afd240a630d5b0d2e3e0f4239e031f30ee2c357f
jansenwt <67090708+jansenwt@users.noreply.github.com>
2021-06-19 12:50:21 +0700

    Material Design, Nav Drawer, Appbar

--- app/src/main/java/id/ac/ubaya/informatika/mylulus06/MainActivity.kt
+++ app/src/main/java/id/ac/ubaya/informatika/mylulus06/MainActivity.kt
@@ -1,17 +1,53 @@
 package id.ac.ubaya.informatika.mylulus06
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.core.view.GravityCompat
 import androidx.fragment.app.Fragment
 import androidx.viewpager2.widget.ViewPager2
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.drawer_layout.*
 
 class MainActivity : AppCompatActivity() {
     val fragments: ArrayList<Fragment> = ArrayList()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.drawer_layout)
+        setSupportActionBar(toolbar)
+        supportActionBar?.title = "MyLulus06"
+        supportActionBar?.setDisplayHomeAsUpEnabled(false)
+
+        var drawerToggle =
+                ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.app_name, R.string.app_name)
+
+        drawerToggle.isDrawerIndicatorEnabled = true
+        drawerToggle.syncState()
+
+        navView.setNavigationItemSelectedListener {
+            viewPager.currentItem = when(it.itemId){
+                R.id.itemMatkul -> 0
+                R.id.itemCekLul -> 1
+                R.id.itemProfil -> 2
+                else -> 0
+            }
+            val sendIntent = Intent().apply {
+                action = Intent.ACTION_SEND
+                putExtra(Intent.EXTRA_EMAIL, arrayOf("mylulus@unit.ubaya.ac.id"))
+                putExtra(Intent.EXTRA_SUBJECT, "Laporan Permasalahan Aplikasi")
+                putExtra(Intent.EXTRA_TEXT, "Terdapat kesalahan pada data saya")
+                type = "message/rfc822"
+            }
+            when(it.itemId){
+                R.id.itemHubungi -> startActivity(Intent.createChooser(sendIntent, "Send Email"));
+            }
+            drawerLayout.closeDrawer(GravityCompat.START)
+            true
+        }
 
         with(fragments){
             add(MatKulFragment())
